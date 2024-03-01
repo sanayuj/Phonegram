4ea3c7dbdf8be9d970fb1544e2b5ee657160f160
sanayuj <sanayuj2255@gmail.com>
2024-03-01 16:20:07 +0530

    user register

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,6 +1,33 @@
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+const userSchema = require("../Models/userModel");
+const { json } = require("express");
 
+module.exports.register = async (req, res, next) => {
+  const { name, email, password } = req.body;
 
+  const userExist = await userSchema.findOne({ email });
 
-module.exports.register=async(req,res,next)=>{
+  if (userExist) {
+    return res.json({ status: false, message: "Account already exists" });
+  }
+  if (!name || !email || !password) {
+    return res.json({ status: false, message: "All fields required" });
+  }
+  if (!validator.isEmail(email)) {
+    return res.json({ status: false, message: "Check Valid email" });
+  }
+  if (!validator.isStrongPassword(password)) {
+    return res.json({ status: false, message: "Password must be strong" });
+  }
 
-}
+  const newUser = new userSchema({
+    name,
+    email,
+    password,
+  });
+
+  const salt=await bcrypt.genSalt(10)
+  newUser.password=await bcrypt.hash(user.password,salt)
+  await newUser.save()
+};
