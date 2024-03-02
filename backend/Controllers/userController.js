ddc4756c88815cd6f3be5b8f6c5674bb7762f0c7
sanayuj <sanayuj2255@gmail.com>
2024-03-02 09:31:52 +0530

    login

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,33 +1,55 @@
 const bcrypt = require("bcrypt");
 const validator = require("validator");
 const userSchema = require("../Models/userModel");
-const { json } = require("express");
+const jwt = require("jsonwebtoken");
+const maxAge = "3d";
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SCERET_KEY, { expireIn: maxAge });
+};
 
 module.exports.register = async (req, res, next) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-  const userExist = await userSchema.findOne({ email });
+    const userExist = await userSchema.findOne({ email });
 
-  if (userExist) {
-    return res.json({ status: false, message: "Account already exists" });
-  }
-  if (!name || !email || !password) {
-    return res.json({ status: false, message: "All fields required" });
-  }
-  if (!validator.isEmail(email)) {
-    return res.json({ status: false, message: "Check Valid email" });
-  }
-  if (!validator.isStrongPassword(password)) {
-    return res.json({ status: false, message: "Password must be strong" });
-  }
+    if (userExist) {
+      return res.json({ status: false, message: "Account already exists" });
+    }
+    if (!name || !email || !password) {
+      return res.json({ status: false, message: "All fields required" });
+    }
+    if (!validator.isEmail(email)) {
+      return res.json({ status: false, message: "Check Valid email" });
+    }
+    if (!validator.isStrongPassword(password)) {
+      return res.json({ status: false, message: "Password must be strong" });
+    }
 
-  const newUser = new userSchema({
-    name,
-    email,
-    password,
-  });
+    const newUser = new userSchema({
+      name,
+      email,
+      password,
+    });
 
-  const salt=await bcrypt.genSalt(10)
-  newUser.password=await bcrypt.hash(user.password,salt)
-  await newUser.save()
+    const salt = await bcrypt.genSalt(10);
+    newUser.password = await bcrypt.hash(newUser.password, salt);
+    await newUser.save();
+    return res.json({status:true,message:"User registered successfully"})
+  } catch (err) {
+    console.log(err);
+  }
 };
+
+
+module.exports.login=async(req,res,next)=>{
+    try{
+        const {email,password}=req.body
+        const userExist=await userSchema.findOne({email})
+        
+
+    }catch(err){
+        console.log(err);
+    }
+}
--- backend/Models/userModel.js
+++ backend/Models/userModel.js
@@ -1,23 +1,23 @@
 const mongoose=require("mongoose")
 
 const userModel=new mongoose.Schema({
-    username:{
+    name:{
         type:String,
         required:true,
-        minlength:3,maxlength:30
+       
     },
     email:{
         type:String,
         required:true,
         unique:true,
-        minlength:3,maxlength:30
+       
     },
     password:{
         type:String,
         required:true,
-        minlength:3,maxlength:30
+       
     }
 })
 
-const userSchema=mongoose.Model("User",userModel)
-module.exports=userSchema
+
+module.exports=new mongoose.model("User",userModel)
