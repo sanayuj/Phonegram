f882d5eb5ab55599bb41eb2e8de63d552600e27f
sanayuj <sanayuj2255@gmail.com>
2024-03-13 15:28:19 +0530

    frontend design

--- frontend/src/Context/ChatContext.jsx
+++ frontend/src/Context/ChatContext.jsx
@@ -7,6 +7,16 @@ export const ChatContextProvider = ({ children, user }) => {
   const [userChats, setUserChats] = useState(null);
   const [isUserChatLoading, setIsUserChatLoading] = useState(false);
   const [userChatError, setUserChatError] = useState(null);
+  const [potentialChats,setPotentialChats]=useState([])
+
+  const getUsers=async()=>{
+    const response=await getRequest(`${baseUrl}/`)
+    if(response.error){
+      return console.log("Error fetching users",response);
+    }
+    setPotentialChats(response)
+
+  }
 
   useEffect(() => {
     const getUserChat = async () => {
@@ -18,16 +28,26 @@ export const ChatContextProvider = ({ children, user }) => {
         if (response.error) {
           return setUserChatError(response);
         }
-
-        setUserChats(response);
+        const pChats=response.filter((U)=>{
+          let isChatCreated=false
+          if(user.Id===U.Id)return false
+          if(userChats){
+           isChatCreated = userChats.some((chat)=>{
+              return chat.members[0]===U.Id || chat.members[1]===U.Id
+            })
+          }
+          !isChatCreated
+        })
+       
       }
     };
     getUserChat()
-  }, [user]);
+    getUsers()
+  }, [userChats]);
 
   return (
     <ChatContext.Provider
-       value={{ userChats, isUserChatLoading, userChatError }}
+       value={{ userChats, isUserChatLoading, userChatError,potentialChats }}
     >
       {children}
     </ChatContext.Provider>
