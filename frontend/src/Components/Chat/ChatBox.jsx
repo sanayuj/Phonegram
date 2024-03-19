8fce60df1985b10b8bc65ca1c06e19934a086e0a
sanayuj <sanayuj2255@gmail.com>
2024-03-19 15:07:27 +0530

    chat listed

--- frontend/src/Components/Chat/ChatBox.jsx
+++ frontend/src/Components/Chat/ChatBox.jsx
@@ -13,14 +13,18 @@ function ChatBox() {
     console.log(recipientUser,"@@@@@2@@@@@@@@@@@@@@@@@@");
     console.log(message,"*******^&&&^&^");
     if(!recipientUser){
+        return(
         <p style={{textAlign:'center',width:"100%"}}>
             No conversation started yet!
         </p>
+        )
     }
     if(isMessageLoading){
+        return(
         <p style={{textAlign:'center',width:"100%"}}>
-            Loading
+            Loading....
         </p>
+        )
     }
   return (
     <Stack gap={4} className='chat-box'>
@@ -29,7 +33,7 @@ function ChatBox() {
         </div>
         <Stack gap={3} className='messages'>
             {message && message?.message?.map((message,index)=>(
-                <Stack key={index} className={`${message?.senderId===user?._id ?"message self align-self-end flex-grow-0":"message align-self-start flex-grow-0"}`}>
+                <Stack key={index} className={`${message?.senderId===user?._id ? "message self align-self-end flex-grow-0":"message align-self-start flex-grow-0"}`}>
                     <span className=''>{message.text}</span>
                     <span className='message-footer'>{moment(message.createdAt).calendar()}</span>
                 </Stack>
